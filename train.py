1794f97ec75a998987c27ee10b1e64cf79ade63f
nozakijpn <nozakiguitar@gmail.com>
2018-06-19 00:31:53 +0900

    remake

--- train.py
+++ train.py
@@ -24,8 +24,12 @@ def replace_csv_data(self):
                 
   
 #replace_csv_data(1)              
+hairetu = []
+hairetu = np.array(hairetu)
+labels = []
+labels = np.array(labels)
 
-for name in range(1,5000):
+for name in range(1,9999):
     for year in range(2015,2017):
         if(os.path.exists('data/{0}_{1}.csv'.format(name,year))==True):
             data = np.genfromtxt("data/rep/{0}_{1}.csv".format(name,year),delimiter=",", skip_header=2, dtype='int')
@@ -35,10 +39,14 @@ for name in range(1,5000):
             maxcnt = int(maxcnt/21)
             #始値 	高値	安値	終値	出来高
             for i in range(0,maxcnt):
-                print(data[i*21:i*21+21])
+                #print(data[i*21:i*21+21].shape)
+                hairetu = np.append(hairetu,np.array(data[i*21:i*21+20]))
                 sub = data[i*21+20,3]-data[i*21+19,3]#最終日とその前日の差
-                if(sub>0):
+                if(sub>0):#値が上がっていればlabelに1を返す
                     label=1
                 else:
                     label=0
-                print(sub)
+                labels = np.append(labels,label)
+traindata = np.reshape(hairetu,(-1,20,5))#trainセットを作成
+print(traindata.shape)#batch,days,features
+print(labels.shape)
